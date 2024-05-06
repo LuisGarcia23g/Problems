323d4662f5b2a8367531bda81b007b2d7bcd6cb7
LuisGarcia23g <164560978+LuisGarcia23g@users.noreply.github.com>
2024-05-06 02:18:14 -0600

    Update Letras y colores.cpp
    Actualización, estoy 30% seguro que agarra.

--- Letras y colores.cpp
+++ Letras y colores.cpp
@@ -8,58 +8,30 @@ int main () {
 int letra, R=1, Y=2, B=3, W=4, K=5, C=6, G=7;
 
 cout << "Escriba una de estas letras: R, Y, B, W, K, C, G ";
-cin >> letra;
-
-if (letra == R == 1)
+cin >> letra 
 
 switch (letra){
     case 1: cout << "Rojo";
     break;
-    }
-else if (letra == Y == 2)
 
-switch (letra){
     case 2: cout << "Amarillo";
     break;
-    }
 
-else if (letra == B == 3)
-
-switch (letra){
     case 3: cout << "Azul";
     break;
-    }
 
-else if (letra == W == 4)
-
-switch (letra){
     case 4: cout << "Blanco";
     break;
-    }
-
-else if (letra == K == 5)
-
-switch (letra){
+    
     case 5: cout << "Negro";
     break;
-    }
-
-else if (letra == C == 6)
-
-switch (letra){
+    
     case 6: cout << "Cian";
     break;
-    }
 
-else if (letra == G == 7)
-
-switch (letra){
     case 7: cout << "Verde";
     break;
-    }
-
-else cout << "Escriba una de las letras y en mayusculas";
-    
-
+    default: cout << "Escriba la letra en mayuscula";
+}
     return 0;
 }
